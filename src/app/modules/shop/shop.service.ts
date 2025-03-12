3e53fa864215121465b418d97ea26eef8c15224f
Roton02 <rph645102@gmail.com>
2025-03-12 15:17:37 +0600

    update cloudinary creadinatial in env

--- src/app/modules/shop/shop.service.ts
+++ src/app/modules/shop/shop.service.ts
@@ -1,27 +1,31 @@
-import mongoose from "mongoose";
-import { IImageFile } from "../../interface/IImageFile";
-import { IShop } from "./shop.interface";
-import { IJwtPayload } from "../auth/auth.interface";
-import User from "../user/user.model";
-import AppError from "../../errors/appError";
-import { StatusCodes } from "http-status-codes";
-import Shop from "./shop.model";
+import mongoose from 'mongoose'
+import { IImageFile } from '../../interface/IImageFile'
+import { IShop } from './shop.interface'
+import { IJwtPayload } from '../auth/auth.interface'
+import User from '../user/user.model'
+import AppError from '../../errors/appError'
+import { StatusCodes } from 'http-status-codes'
+import Shop from './shop.model'
 
-const createShop = async (shopData: Partial<IShop>, logo: IImageFile, authUser: IJwtPayload) => {
-
-  const session = await mongoose.startSession();
-  session.startTransaction();
+const createShop = async (
+  shopData: Partial<IShop>,
+  logo: IImageFile,
+  authUser: IJwtPayload
+) => {
+  const session = await mongoose.startSession()
+  session.startTransaction()
 
   try {
+    // console.log(shopData, logo)
     // Check if the user already exists by email
-    const existingUser = await User.findById(authUser.userId).session(session);
+    const existingUser = await User.findById(authUser.userId).session(session)
 
     if (!existingUser) {
-      throw new AppError(StatusCodes.NOT_ACCEPTABLE, 'User is not exists!');
+      throw new AppError(StatusCodes.NOT_ACCEPTABLE, 'User is not exists!')
     }
 
     if (!existingUser.isActive) {
-      throw new AppError(StatusCodes.NOT_ACCEPTABLE, 'User is not active!');
+      throw new AppError(StatusCodes.NOT_ACCEPTABLE, 'User is not active!')
     }
 
     if (logo) {
@@ -30,40 +34,40 @@ const createShop = async (shopData: Partial<IShop>, logo: IImageFile, authUser: 
 
     const shop = new Shop({
       ...shopData,
-      user: existingUser._id
-    });
+      user: existingUser._id,
+    })
 
-    const createdShop = await shop.save({ session });
+    const createdShop = await shop.save({ session })
 
     await User.findByIdAndUpdate(
       existingUser._id,
       { hasShop: true },
       { new: true, session }
-    );
+    )
 
     // Commit the transaction
-    await session.commitTransaction();
-    session.endSession();
+    await session.commitTransaction()
+    session.endSession()
 
-    return createdShop;
+    return createdShop
   } catch (error) {
-    await session.abortTransaction();
-    session.endSession();
-    throw error;
+    await session.abortTransaction()
+    session.endSession()
+    throw error
   }
-};
+}
 
 const getMyShop = async (authUser: IJwtPayload) => {
-  const existingUser = await User.checkUserExist(authUser.userId);
+  const existingUser = await User.checkUserExist(authUser.userId)
   if (!existingUser.hasShop) {
-    throw new AppError(StatusCodes.NOT_FOUND, "You have no shop!")
+    throw new AppError(StatusCodes.NOT_FOUND, 'You have no shop!')
   }
 
-  const shop = await Shop.findOne({ user: existingUser._id }).populate('user');
-  return shop;
+  const shop = await Shop.findOne({ user: existingUser._id }).populate('user')
+  return shop
 }
 
 export const ShopService = {
   createShop,
-  getMyShop
+  getMyShop,
 }
